aa2d467a35bde2fd9e893362928be219cd55e404
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-06-17 15:14:59 +0200

    add functionality for keydown

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('wheel', (event) => scroller.listenScroll(event));
   document.addEventListener('swipeUp', () => scroller.scrollDirection(1));
   document.addEventListener('swipeDown', () => scroller.scrollDirection(-1));
+  document.addEventListener('keydown', (event) => {
+    switch (event.keyCode) {
+      case 40:
+        return scroller.scrollDirection(1);
+      case 38:
+        return scroller.scrollDirection(-1);
+
+      default:
+        return;
+    }
+  });
 });
